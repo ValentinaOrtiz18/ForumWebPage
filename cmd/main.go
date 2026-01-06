7799a7112b919b743f58e0817bb501d2c2ddf33c
Student <student@mx23-hp>
2026-01-06 20:44:13 +0000

    qazinto12

--- cmd/main.go
+++ cmd/main.go
@@ -3,16 +3,17 @@ package main
 import (
 	"forum/internal/database"
 	"forum/internal/handlers"
+	"log"
 	"net/http"
 )
 
 func main() {
 	// Initialize the database
-	database.InitDB("forum.DB")
+	database.InitDB("forum.db")
 
 	// Set up routes
 	http.HandleFunc("/", handlers.IndexHandler)
-	http.HandleFunc("/", handlers.HomeHandler)
+	http.HandleFunc("/index", handlers.HomeHandler)
 	http.HandleFunc("/login", handlers.LoginHandler)
 	http.HandleFunc("/logout", handlers.LogoutHandler)
 	http.HandleFunc("/post", handlers.ViewPostHandler)
@@ -20,6 +21,12 @@ func main() {
 	http.HandleFunc("/comment/create", handlers.CreateCommentHandler)
 	http.HandleFunc("/register", handlers.RegisterHandler)
 
+	http.Handle("/static/",
+		http.StripPrefix("/static/",
+			http.FileServer(http.Dir("static")),
+		),
+	)
 	// Start server
-	http.ListenAndServe(":8080", nil)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
 }
